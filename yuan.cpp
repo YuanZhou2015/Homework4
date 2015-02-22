8e76b45ad264f989d1ac205b84e1494957f0daa2
YuanZhou2015 <yuanzhou0924@gmail.com>
2015-02-22 15:18:37 -0600

    Add another message

--- yuan.cpp
+++ yuan.cpp
@@ -9,14 +9,17 @@ using namespace std;
 
 int main() {
 
-    cout << "I was able to compile this code using the HPC at the University of Memphis.";
-    cout << "When I compiled it there, it did not produce any warning message.";
-    cout << "The HPC uses a GNU C++ compiler that can be considered ";
-    cout << "a good up-to-date standard. I also version-controlled ";
-    cout << "this code using git, ";
-    cout << "and use a remote repository hosted by github. ";
-    cout << "If I can do this, so can you!!!";
-
+    cout << "I was able to compile this code using the HPC at the University of Memphis."<< "\n";
+    cout << "When I compiled it there, it did not produce any warning message."<< "\n";
+    cout << "The HPC uses a GNU C++ compiler that can be considered "<< "\n";
+    cout << "a good up-to-date standard."<< "\n";
+    cout << "I also version-controlled this code using git,"<< "\n";
+    cout << "and use a remote repository hosted by github. "<< "\n";
+    cout << "If I can do this, so can you!!!" << "\n";
+    cout << endl;
+    cout << "I am so cool, that I was also able to write a code " << "\n";
+    cout << "that produces the first N numbers of the Fibonacci sequence." << "\n";
+    cout << "Here they are:"<< "\n";
     return 0;
 
 }
