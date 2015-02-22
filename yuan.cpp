7880f88f3292ee8cc9938a7fbf77e03642d4bb04
YuanZhou2015 <yuanzhou0924@gmail.com>
2015-02-22 15:03:53 -0600

    Modify code
    Add a message.

--- yuan.cpp
+++ yuan.cpp
@@ -8,6 +8,15 @@
 using namespace std;
 
 int main() {
+
+    cout << "I was able to compile this code using the HPC at the University of Memphis.";
+    cout << "When I compiled it there, it did not produce any warning message.";
+    cout << "The HPC uses a GNU C++ compiler that can be considered ";
+    cout << "a good up-to-date standard. I also version-controlled ";
+    cout << "this code using git, ";
+    cout << "and use a remote repository hosted by github. ";
+    cout << "If I can do this, so can you!!!";
+
     return 0;
 
 }
