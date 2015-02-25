831716b20dea6a7197af8da4ca3ded5bda6a4ef2
YuanZhou2015 <yuanzhou0924@gmail.com>
2015-02-24 21:52:31 -0600

    Final version

--- yuan.cpp
+++ yuan.cpp
@@ -10,79 +10,97 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <iomanip>
 
 using namespace std;
 
-void print_output(ofstream &outputfile, ofstream &errorfile,string str){
-    string Fibornacci;
-    if (str == Fibornacci){
-       cout << str;
-       outputfile << str;
+//Print out function for print out different information and Fibonacci sequence.
+
+void print_output(ofstream &outputfile, ofstream &errorfile,string str,int &F){
+    
+    if (F==1){
+        outputfile.open("yuan.out");
+        cout << str;
+        outputfile << str;
+        outputfile.close();
     }
-    else {
+
+    if (F==2){
+        errorfile.open("yuan.err");
         cout << str;
         errorfile << str;
-    }
+        errorfile.close();
+    }    
+     
 }
 
+// The funcitons to open and check the files.
 
-ifstream &open_input(string inputfilename,ifstream &inputfile,int &flag, int &N){
+void open_input(string inputfilename,ifstream &inputfile,int &flag, int &N){
 
     ofstream outputfile, errorfile;
     stringstream ss;
     string information;
 
-    inputfile.open(inputfilename);
+    inputfile.open(inputfilename.c_str());
 
-    errorfile.open("yuan.err");
     if (!inputfile.is_open()){
         ss << "Cannot open input file: "
-                    << inputfilename
-                    << endl;
+           << inputfilename
+           << endl;
         information = ss.str();
-        print_output(outputfile,errorfile,information);
+        int F = 1;
+        print_output(outputfile,errorfile,information,F);
         flag = 1;
-        return(inputfile);
+        return;
     }
 
     // Check if the N is legal to create the Fibonacci sequence.
 
     inputfile >> N;
     if (N<1) {
-        ss << "The input N is invalid. "
-             << endl;
+        ss << "The input N is invalid. \n";
         information = ss.str();
-        print_output(outputfile,errorfile,information);
+        int F=2;
+        print_output(outputfile,errorfile,information,F);
         flag = 1;
-        return(inputfile);
+        return;
     }
  
-    return(inputfile);
+    return;
 }
 
-ofstream &open_error( string errorfilename, ofstream &errorfile){
-    errorfile.open(errorfilename);
+void open_error( string errorfilename, ofstream &errorfile){
+    stringstream ss;
+    string information;
+    errorfile.open(errorfilename.c_str());
     if (!errorfile.is_open() ){
-        cout << "Cannot open error file: "
+        ss << "Cannot open error file: "
              << errorfilename
              << endl;
-        return (errorfile);
+        information = ss.str();
+        int F=2;
+        print_output(errorfile,errorfile,information,F);
+        int flag = 1;
+        return;
     }
 }
 
-ofstream &open_output( string outputfilename, ofstream &outputfile){
+void open_output( string outputfilename, ofstream &outputfile){
     ofstream errorfile;
-    outputfile.open(outputfilename);
+    stringstream ss;
+    string information;
+
+    outputfile.open(outputfilename.c_str());
     errorfile.open("yuan.err");
     if (!outputfile.is_open() ){
-        cout << "Cannot open output file: "
+        ss << "Cannot open output file: "
              << outputfilename
              << endl;
-        errorfile <<"Cannot open output file: "
-                  << outputfilename
-                  << endl;
-        return (outputfile);
+        information = ss.str();
+        int F=2;
+        print_output(outputfile,errorfile,information,F);
+        int flag = 1;
+        return;
     }
 }
 
@@ -109,19 +127,21 @@ int main() {
     ifstream inputfile;
     ofstream outputfile, errorfile;
     string inputfilename, outputfilename, errorfilename;
-    int N,i,j,flag=0;
+    int N,i,flag=0;
 
     // Open error file, output file, input file by calling functions
 
     open_error("yuan.err",errorfile);
     errorfile.close();
-    open_output("yuan.out",outputfile);    
+    open_output("yuan.out",outputfile); 
+    outputfile.close();
     open_input("yuan.in", inputfile, flag, N);  
 
+    // If any of these files fail to be opened or has other error, exit the program.
     if (flag == 1)
     return 0;   
 
-    unsigned long long temp=0,f1, f2;
+    unsigned long long temp=0, f1, f2;
     string  Fibonacci;
     stringstream ss;
     ss << "The first " << N*10 <<" numbers of Fibonacci sequence are:" << "\n";
@@ -142,11 +162,12 @@ int main() {
             ss << temp<<"  ";
         Fibonacci = ss.str();      
     } 
-    print_output(outputfile,errorfile, Fibonacci);
-   
+    int F=1;
+    print_output(outputfile,errorfile, Fibonacci,F);
+
     inputfile.close();
     outputfile.close();
     errorfile.close();
+    
     return 0;
-
 }
